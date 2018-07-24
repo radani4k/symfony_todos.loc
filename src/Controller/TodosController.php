d536dc8864a04db0c7a3593f8927c9681c81debf
radan4ik <13579qetup>
2018-07-24 23:11:02 +0300

    add registration and authentication

--- src/Controller/TodosController.php
+++ src/Controller/TodosController.php
@@ -11,7 +11,7 @@ class TodosController extends AbstractController
 {
 
     /**
-     * @Route("/" , name="homepage")
+     * @Route("/", name="homepage")
      */
     public function homepage()
     {
